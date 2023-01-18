ba89dd3fee77b4f7b7c58e470bee694cb64e50e3
matheussa150201 <matheussadejesus123@gmail.com>
2023-01-18 17:26:47 -0300

    NEW_000 - Criacao do projeto

---
+++ src/main/java/com/sistema/sa/bff/SistemaSaBffApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.sa.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSaBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaSaBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema/sa/bff/SistemaSaBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.sa.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaSaBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
